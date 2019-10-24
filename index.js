8f6a021ce0a3122e31c9e4c4a3339b2fd49c88bd
may <maywanting@gmail.com>
2019-10-25 08:01:24 +0900

    add link

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let svg = d3.select("#chart").append("svg").attr("width", 1000).attr("height", 1000);
 
 
-function transPosition(r, angle) {
-    let rx = Math.sin(angle) * r;
-    let ry = 0 - Math.cos(angle) * r;
+function transPosition(r, angle, x = 0, y = 0) {
+    let rx = Math.sin(angle) * r + x;
+    let ry = 0 - Math.cos(angle) * r + y;
     return {"x" : rx, "y" : ry};
 }
 function drawPath(output, input, group) {
@@ -14,11 +14,11 @@ function drawPath(output, input, group) {
     let p_d = transPosition(95, output.endAngle);
 
     let MA = "M" + p_a.x + "," + p_a.y;
-    let QAB = " Q0,0" + p_b.x + "," + p_b.y;
+    let QAB = " Q0,0," + p_b.x + "," + p_b.y;
     let LBE = " L" + p_e.x + "," + p_e.y;
     let LEC = " L" + p_c.x + "," + p_c.y;
     // let ABC = " A94,94,0,0,0," + p_c.x + "," + p_c.y;
-    let QCD = " Q0,0" + p_d.x + "," + p_d.y;
+    let QCD = " Q0,0," + p_d.x + "," + p_d.y;
     let ADA = " A94,94,0,0,0," + p_a.x + "," + p_a.y;
 
     let pathDetail = MA + QAB + LBE + LEC + QCD + ADA;
@@ -94,6 +94,7 @@ function createChord(dataset) {
     drawPath(output, input, group);
     drawPath(data0[3], data0[9], group);
 
+    return pie;
 }
 // group.append("path").attr("d", pathDetail);
 
@@ -120,5 +121,45 @@ let dataset2 = {
         [0, 0, 5, 2, 0],
     ]
 };
-createChord(dataset1);
-createChord(dataset2);
+let chord1 = createChord(dataset1);
+let chord2 = createChord(dataset2);
+
+console.log(chord1);
+
+function calculateArcP (x1, y1, x2, y2, x3, y3) {
+    let x = x1 + x3 - x2;
+    let y = y1 + y3 - y2;
+    return {"x": x, "y": y};
+}
+//chord1[0] -> chord2[1];
+let link1 = svg.append("g");
+
+let output = chord1[3];
+let input = chord2[1];
+
+let A1 = transPosition(100, output.endAngle, dataset1.x, dataset1.y);
+let B1 = transPosition(100, output.startAngle, dataset1.x, dataset1.y);
+let D1 = transPosition(100, (output.startAngle + output.endAngle) / 2.0, dataset1.x, dataset1.y);
+let C1 = transPosition(100 * 2, (output.startAngle + output.endAngle) / 2.0, dataset1.x, dataset1.y);
+let C2 = calculateArcP(A1.x, A1.y, D1.x, D1.y, C1.x, C1.y);
+let C3 = calculateArcP(B1.x, B1.y, D1.x, D1.y, C1.x, C1.y);
+
+
+let A2 = transPosition(100 * 1.1, input.endAngle, dataset2.x, dataset2.y);
+let B2 = transPosition(100 * 1.1, input.startAngle, dataset2.x, dataset2.y);
+let D2 = transPosition(100, ( input.startAngle + input.endAngle) / 2.0, dataset2.x, dataset2.y);
+let E1 = transPosition(100 * 2,  ( input.startAngle + input.endAngle) / 2.0, dataset2.x, dataset2.y);
+let E2 = calculateArcP(A2.x, A2.y, D2.x, D2.y, E1.x, E1.y);
+let E3 = calculateArcP(B2.x, B2.y, D2.x, D2.y, E1.x, E1.y);
+
+let MA1 = "M" + A1.x + "," + A1.y;
+let CA1A2 = " C" + C2.x + "," + C2.y + "," + E2.x + "," + E2.y + "," + A2.x + "," + A2.y;
+let LA2D2 = " L" + D2.x + "," + D2.y;
+let LD2B2 = " L" + B2.x + "," + B2.y;
+let CB2B1 = " C" + E3.x + "," + E3.y + "," + C3.x + "," + C3.y + "," + B1.x + "," + B2.y;
+let AB1A1 = " L" + A1.x + "," + A1.y;
+// let AB1A1 = " A100,100,0,0,1," + A1.x + "," + A1.y;
+
+let path = MA1 + CA1A2 + LA2D2 + LD2B2 + CB2B1 + AB1A1;
+link1.append("g").attr("class", "link")
+    .append("path").attr("d", path).attr("fill", "green");
